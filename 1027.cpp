bf81fede67b8fdeb170d36372df3925c71ef45bc
Patrick233 <patrick4dev@foxmail.com>
2018-06-22 17:17:58 +0800

    update simple codes

---
+++ 1027.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, const char * argv[]) {
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	if (b * b - 4 * a * c >= 0)
+		std::cout << "YES\n";
+	else
+		std::cout << "NO SOLUTION\n";
+	return 0;
+}
