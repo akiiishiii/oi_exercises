46fd01c3ea2fb5bc4743680c323a668624ea14ad
Patrick233 <patrick4dev@foxmail.com>
2018-06-22 14:32:40 +0800

    update simple codes

---
+++ 1003.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	long long a, b, c;
+	std::cin >> a >> b >> c;
+	std::cout << (a + b) / c;
+	return 0;
+}
---
+++ 1004.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, const char * argv[]) {
+	long long a, b;
+	std::cin >> a >> b;
+	std::cout << std::fixed << std::setprecision(3) << b * 100 / double(a) << "%\n";
+	return 0;
+}
