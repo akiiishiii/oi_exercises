b1020d534b20d5af0b4989b0588384e0fab8a5ae
Patrick233 <patrick4dev@foxmail.com>
2018-06-22 16:41:12 +0800

    update simple codes

---
+++ 1023.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	if (a + b > c && a + c > b && b + c > a)
+		std::cout << "yes\n";
+	else
+		std::cout << "no\n";
+	return 0;
+}
