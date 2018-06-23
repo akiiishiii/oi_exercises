5a52df4e6cb454127c8280064c0f1865422aa7f9
Patrick233 <patrick4dev@foxmail.com>
2018-06-23 11:15:08 +0800

    update codes

---
+++ 1049.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	int m, n;
+	std::cin >> m >> n;
+	if (!(m % 2))
+		m++;
+	if (!(n % 2))
+		n--;
+	if (m > n) {
+		std::cout << "0\n";
+		return 0;
+	}
+	std::cout << (m + n) * (n - m - (n - m - 1) / 2) / 2 << std::endl;
+	return 0;
+}
---
+++ 1053.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	int n;
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++)
+			std::cout.put(' ');
+		for (int j = 0; j < 2 * n - 1 - 2 * i; j++)
+			std::cout.put('*');
+		std::cout << std::endl;
+	}
+	return 0;
+}
