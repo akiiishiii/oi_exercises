f3ea6702d70d3cc82c54ec0a8c52fcabc8e03194
Patrick233 <patrick4dev@foxmail.com>
2018-06-24 11:23:59 +0800

    updates

--- 1073.cpp
+++ 1073.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-
-struct pos{
-	int a;
-	
-};
+#include <algorithm>
+#include <vector>
 
 int main(int argc, const char * argv[]) {
 	int n, mat[21][21];
-	
+	bool 
 	std::cin >> n;
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
-			std::cin >> n[i][j];
-	
+			std::cin >> mat[i][j];
+	for (int i = 1; i <= n; i++)
+		
+	for (int i = 1; i <= n; i++) {
+		
+	}
 	return 0;
 }
---
+++ 1074.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+struct Student {
+	double score[3];
+	double avr;
+	void get_avr();
+};
+
+std::ostream & operator<<(std::ostream & os, Student & s);
+std::istream & operator>>(std::istream & is, Student & s);
+
+int main(int argc, const char * argv[]) {
+	int n;
+	std::vector<Student> st;
+	std::cin >> n;
+	st.resize(n);
+	std::cout << std::fixed << std::setprecision(2);
+	for (int i = 0; i < n; i++) {
+		std::cin >> st[i];
+		std::cout << st[i] << ' ';
+	}
+	std::cout << std::endl;
+	return 0;
+}
+
+void Student::get_avr() {
+	avr = (score[0] + score[1] + score[2]) / 3;
+	return;
+}
+
+std::ostream & operator<<(std::ostream & os, Student & s) {
+	s.get_avr();
+	os << s.avr;
+	return os;
+}
+
+std::istream & operator>>(std::istream & is, Student & s) {
+	for (int i = 0; i < 3; i++)
+		is >> s.score[i];
+	return is;
+}
