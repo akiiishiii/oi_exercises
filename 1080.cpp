ca7c76a759dbe05084538e38c2c511552bdd9251
Patrick233 <patrick4dev@foxmail.com>
2018-06-25 17:27:46 +0800

    updates

--- 1080.cpp
+++ 1080.cpp
@@ -1,10 +1,11 @@
 // 1080.cpp
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, const char * argv[]) {
     int k, n;
     double x, y;
     std::cin >> k >> n >> x >> y;
-
+    std::cout << std::fixed << std::setprecision(2) << (n <= k ? n * x : k * x + (n - k) * y);
     return 0;
 }
