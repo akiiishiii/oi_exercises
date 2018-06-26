ee2549a9ebf643fa37829e4ff517397c2ee90984
Patrick233 <patrick4dev@foxmail.com>
2018-06-26 09:29:38 +0800

    updates

--- 1084.cpp
+++ 1084.cpp
@@ -1,12 +1,20 @@
 // 1084.cpp
 #include <iostream>
+#include <cmath>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    int n, m, res = 0, temp;
+    int n, res = 0;
+    std::string m, temp;
     std::cin >> n >> m;
     temp = m;
-    while (temp) {
-
+    for (int i = 0; !temp.empty(); i++) {
+        if (temp.back() >= 'A' && temp.back() <= 'F')
+            res += int(temp.back() - 'A' + 10) * pow(n, i);
+        else
+            res += int(temp.back() - '0') * pow(n, i);
+        temp.erase(temp.end() - 1);
     }
+    std::cout << "(" << m << ")" << n << "=(" << res << ")10" << std::endl;
     return 0;
 }
