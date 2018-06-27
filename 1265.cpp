f2c3f84804631392180a955038b8fe0bedc31871
Patrick233 <patrick4dev@foxmail.com>
2018-06-27 16:52:48 +0800

    updates

--- 1265.cpp
+++ 1265.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 
 int main(int argc, const char *argv[]) {
-    long long x, y, z, f[55], s[55];
+    int x, y, z;
+    long long f[55] = {0}, c[55] = {0};
     std::cin >> x >> y >> z;
-    for (int i = 0; i <= x; i++) {
+    for (int i = 1; i <= x; i++) {
         f[i] = 1;
-        s[i] = 0;
+        c[i] = 0;
     }
     for (int i = x + 1; i <= z + 1; i++) {
-        s[i] = f[i - x] * y;
-        f[i] = f[i - 1] + s[i - 2];
+        c[i] = f[i - x] * y;
+        f[i] = f[i - 1] + c[i - 2];
     }
     std::cout << f[z + 1] << std::endl;
     return 0;
