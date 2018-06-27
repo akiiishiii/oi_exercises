a27919b81e3ec45bcf1408f31c5e30e40a258606
Patrick233 <patrick4dev@foxmail.com>
2018-06-27 21:02:57 +0800

    update

--- 1272.cpp
+++ 1272.cpp
@@ -1,14 +1,14 @@
 // 1272.cpp
 #include <iostream>
-#include <cstring>
 
 int main(int argc, const char * argv[]) {
     int n, f[1001][1001];
-    memset(f, 1, sizeof(f));
     std::cin >> n;
+    for (int i = 1; i <= n; i++)
+        f[1][i] = f[i][1] = 1;
     for (int i = 2; i <= n; i++)
         for (int j = 2; j <= n; j++)
-            f[i][j] = (f[i][j - 1] % 100003 + f[i - 1][j] % 100003) % 100003;
+            f[i][j] = (f[i][j - 1] + f[i - 1][j]) % 100003;
     std::cout << f[n][n] << std::endl;
     return 0;
 }
