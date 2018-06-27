99685256186b1e737f16039b000450233a0a8873
Patrick233 <patrick4dev@foxmail.com>
2018-06-27 17:11:28 +0800

    update

--- 1266.cpp
+++ 1266.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-
+    int n, ans = 0;
+    std::cin >> n;
+    for (int i = 1; i < n; i += 2) {
+        ans += n - i;
+        ans %= 12345;
+    }
+    std::cout << ans % 12345 << std::endl;
     return 0;
 }
