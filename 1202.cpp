2e0e9cb96e2250087aa1e9fb6915d3b351f34dde
Patrick233 <patrick4dev@foxmail.com>
2018-06-28 21:46:36 +0900

    updates

---
+++ 1202.cpp
@@ -0,0 +1,24 @@
+// 1202.cpp
+#include <iostream>
+#include <queue>
+
+void output(int a, int b) {	std::cout << a << ':' << b << std::endl;}
+void clear(int &a, int &b) { a = b = 0;}
+
+int main(int argc, const char * argv[]) {
+	char c;
+	int a = 0, b = 0;
+	while ((c = std::cin.get()) != 'E') {
+		if (c == 'W')
+			a++;
+		else if (c == 'L')
+			b++;
+		if ((a == 11 && b < 10) || (a < 10 && b == 11) || (a >= 10 && b >= 10 && ((a - b) == 2 || (b - a == 2)))) {
+			output(a, b);
+			clear(a, b);
+		}
+	}
+	if (a || b)
+		output(a, b);
+	return 0;
+}
