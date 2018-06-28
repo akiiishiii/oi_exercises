a2de17d83c3150a30599786e865d1ab7ffbc66e0
Patrick233 <patrick4dev@foxmail.com>
2018-06-28 20:03:42 +0900

    updates

---
+++ 1224.cpp
@@ -0,0 +1,9 @@
+// 1224.cpp
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int m, n;
+    std::cin >> n >> m;
+
+    return 0;
+}
---
+++ AtCoder/101C.cpp
@@ -0,0 +1,12 @@
+// 101C.cpp
+#include <iostream>
+#include <vector>
+
+int main(int argc, const char * argv[]) {
+    int k, n;
+    std::vector<int> a;
+    std::cin >> k >> n;
+    a.resize(n);
+
+    return 0;
+}
