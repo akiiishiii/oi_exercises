27a2400f4b8df47842454a2a3a4d3d520148a94c
Patrick233 <patrick4dev@foxmail.com>
2018-06-28 16:30:52 +0800

    updates

---
+++ 1282.cpp
@@ -0,0 +1,13 @@
+// 1282.cpp
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main(int argc, const char * argv[]) {
+    std::__cxx11::basic_string<char> cc;
+    int m;
+    std::cin >> cc >> m;
+    std::sort(cc.begin(), cc.end());
+
+    return 0;
+}
