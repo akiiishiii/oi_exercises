1d7cb3d627cecdc2479b71186d117b834dcd4521
Patrick233 <patrick4dev@foxmail.com>
2018-06-29 12:22:53 +0900

    updates

--- 1226.cpp
+++ 1226.cpp
@@ -1,5 +1,28 @@
 // 1226.cpp
 #include <iostream>
-int main(int argc, const char * argv[]) {
+#include <cstring>
 
+int main(int argc, const char * argv[]) {
+    long long t, n, m, x1, y1, x2, y2, map[31][31];
+    memset(map, 0, sizeof(map));
+    std::cin >> t >> m >> n;
+    map[1][1] = 1;
+    if (t == 1) {
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (i == 1 && j == 1)
+                    continue;
+                else
+                    map[i][j] = map[i - 1][j] + map[i][j - 1];
+    } else {
+        std::cin >> x1 >> y1 >> x2 >> y2;
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if ((i == 1 && j == 1) || (((i <= y1 && i >= y2) || (i >= y1 && i <= y2)) && ((j <= x1 && j >= x2) || (j >= x1 && j <= x2))))
+                    continue;
+                else
+                    map[i][j] = map[i - 1][j] + map[i][j - 1];
+    }
+    std::cout << map[m][n] << std::endl;
+    return 0;
 }
