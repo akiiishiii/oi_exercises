f3667142b209ffae55680d7cf7d22435118a1e9a
Patrick233 <patrick4dev@foxmail.com>
2018-07-05 17:31:45 +0800

    updates

---
+++ 1127.cpp
@@ -0,0 +1,11 @@
+// 1127.cpp
+#include <iostream>
+
+int stair[100001] = {0};
+
+int main(int argc, const char * argv[]) {
+    int n, k;
+    std::cin >> n >> k;
+
+    return 0;
+}
--- 1270.cpp
+++ 1270.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-
+    int f[1001][2], n;
+    std::cin >> n;
+    f[1][0] = 3;
+    f[1][1] = 1;
+    for (int i = 2; i <= n; i++) {
+        f[i][0] = (3 * f[i - 1][0] % 12345 + 2 * f[i - 1][1] % 12345) % 12345;
+        f[i][1] = (f[i - 1][0] % 12345 + f[i - 1][1] % 12345) % 12345;
+    }
+    std::cout << (f[n][0] + f[n][1]) % 12345 << std::endl;
     return 0;
 }
