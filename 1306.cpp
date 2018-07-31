31a8ae082af9fcb456a9d8a9e9c121a32d4b076d
Patrick233 <patrick4dev@foxmail.com>
2018-07-31 11:54:46 +0800

    updates

---
+++ 1306.cpp
@@ -0,0 +1,10 @@
+// 1306.cpp
+#include <iostream>
+
+int arr[7] = {0};
+
+int main(int argc, char const * argv[]) {
+    for (int i = 1; i <= 6; i++)
+        std::cin >> arr[i];
+    return 0;
+}
---
+++ 1480.cpp
@@ -0,0 +1,8 @@
+// 1480.cpp
+#include <iostream>
+
+int main(int argc, char const * argv[]) {
+    int n;
+
+    return 0;
+}
