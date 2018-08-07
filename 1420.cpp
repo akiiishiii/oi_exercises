a6f16149991f4fa215093cecb7170c3b409710ec
Patrick233 <patrick4dev@foxmail.com>
2018-08-07 16:46:03 +0800

    updates

--- 1420.cpp
+++ 1420.cpp
@@ -1,9 +1,29 @@
 // 1420.cpp
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <functional>
 
 int main(int argc, char const *argv[]) {
-    std::priority_queue<int> q;
-    int n, m;
+    std::priority_queue<int, std::vector<int>, std::greater<int> > q;
+    int n, ans = 0;
+    std::cin >> n;
+    for (int temp, i = 0; i < n; i++) {
+        std::cin >> temp;
+        q.push(temp);
+    }
+    while (!q.empty()) {
+        int a, b;
+        a = q.top();
+        q.pop();
+        if (q.empty()) {
+            std::cout << ans << '\n';
+            break;
+        }
+        b = q.top();
+        q.pop();
+        q.push(a + b);
+        ans += a + b;
+    }
     return 0;
 }
---
+++ 1562.cpp
@@ -0,0 +1,8 @@
+// 1562.cpp
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    int n;
+    for ()
+    return 0;
+}
