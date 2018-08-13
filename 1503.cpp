e12ff150fd68466116e3f5fb9ba10cfb3b15227e
Patrick233 <patrick4dev@foxmail.com>
2018-08-13 16:59:44 +0800

    updates

---
+++ 1503.cpp
@@ -0,0 +1,34 @@
+// 1503.cpp
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    int n, k, arr[200005] = {0}, len = 0, lenn = 1, copy[200005] = {0}, d[200005] = {0};
+    std::cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+        std::cin >> arr[i];
+    for (int i = 1; i <= n; i++) {
+        if (i < k && arr[i] < arr[k])
+            copy[++len] = arr[i];
+        else if (i > k && arr[i] > arr[k])
+            copy[++len] = arr[i];
+    }
+    d[1] = copy[1];
+    for (int i = 2; i <= n; i++) {
+        int l = 1, r = lenn;
+        if (d[lenn] < copy[i]) {
+            lenn++;
+            d[lenn] = copy[i];
+            continue;
+        }
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (d[mid] < copy[i])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        d[l] = copy[i];
+    }
+    std::cout << lenn << '\n';
+    return 0;
+}
