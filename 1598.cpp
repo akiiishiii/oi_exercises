04f81e2721fa41f5d7c3e5696bdc241f00dc1712
Patrick233 <patrick4dev@foxmail.com>
2018-09-11 14:34:50 +0800

    updates

--- 1598.cpp
+++ 1598.cpp
@@ -1,12 +1,12 @@
-// 1597.cpp
+//1598.cpp
 #include <queue>
 #include <cstring>
 #include <iostream>
 #include <algorithm>
 
-int const Maxn = 1003, Maxm = 6666;
-int head[Maxn], ver[Maxm], edge[Maxm], Next[Maxm], d[Maxn], used[Maxn], s[Maxn];
-int n, nn, ss, tot, a0, b0, l0, a1, b1, l1;
+int const Maxn = 30, Maxm = 101;
+int head[Maxn], ver[Maxm], edge[Maxm], Next[Maxm], d[Maxn], used[Maxn], r[Maxn], t[Maxn] = {0};
+int n, sum, tot, cnt = 0;
 std::queue<int> q;
 bool v[Maxn];
 
@@ -18,33 +18,32 @@ bool spfa(int mx);
 int main(int argc, char const *argv[]) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
+    for (int i = 1; i <= 24; i++)
+        read(r[i]);
     read(n);
-    read(a0);
-    read(b0);
-    read(l0);
-    read(a1);
-    read(b1);
-    read(l1);
-    nn = n + 2, ss = n + 1;
-    for (int i = 1; i <= n; i++) {
-        add(i, i - 1, 0);
-        add(i - 1, i, 1);
+    for (int i = 1, m; i <= n; i++) {
+        read(m);
+        t[m + 1]++;
+        cnt++;
     }
-    add(ss, 0, 0);
-    for (int i = l0; i <= n; i++) {
-        add(i, i - l0, -(l0 - b0));
-        add(i - l0, i, +(l0 - a0));
-    }
-    for (int i = l1; i <= n; i++) {
-        add(i, i - l1, -(a1));
-        add(i - l1, i, +(b1));
-    }
-    if (spfa(ss))
-        write(d[n] - d[0]);
-    else {
-        std::cout.put('-');
-        std::cout.put('1');
-    }
+    for(sum = 0; sum <= cnt; sum++)
+        if (spfa(24)) {
+            write(sum);
+            std::cout.put('\n');
+            return 0;
+        }
+    std::cout.put('N');
+    std::cout.put('o');
+    std::cout.put(' ');
+    std::cout.put('S');
+    std::cout.put('o');
+    std::cout.put('l');
+    std::cout.put('u');
+    std::cout.put('t');
+    std::cout.put('i');
+    std::cout.put('o');
+    std::cout.put('n');
+    std::cout.put('!');
     std::cout.put('\n');
     return 0;
 }
@@ -74,9 +73,21 @@ void write(int x) {
 }
 
 bool spfa(int mx) {
+    tot = 0;
+    add(24, 0, -sum);
+    for (int i = 0; i <= 23; i++)
+        add(i + 1, i, 0);
+    for (int i = 0; i <= 23; i++)
+        add(i, i + 1, t[i]);
+    for (int i = 8; i <= 24; i++)
+        add(i, i - 8, -r[i]);
+    for (int i = 1; i <= 7; i++)
+        add(i, i + 16, sum - r[i]);
     memset(d, 0x3f, sizeof(d));
+    memset(used, 0, sizeof(used));
     memset(v, false, sizeof(v));
-    d[mx] = 0; v[mx] = true;
+    while(q.size()) q.pop();
+    d[mx] = 0; v[mx] = true; used[mx]++;
     q.push(mx);
     while (q.size()) {
         int x = q.front(); q.pop();
@@ -86,13 +97,10 @@ bool spfa(int mx) {
             if (d[y] > d[x] + z) {
                 d[y] = d[x] + z;
                 used[y]++;
-                if (used[y] >= Maxn) return false;
+                if (used[y] > 25) return false;
                 if (!v[y]) q.push(y), v[y] = true;
             }
         }
     }
-    for (int i = 0; i < nn; i++)
-        if (d[i] > (1 << 29))
-            return false;
-    return true;
+    return d[0] == -sum;
 }
