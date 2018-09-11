f9cbecc4355bf9b2027a5ad8d3c640976ff3d281
Patrick233 <patrick4dev@foxmail.com>
2018-09-11 22:00:25 +0800

    updates

--- 1627.cpp
+++ 1627.cpp
@@ -3,48 +3,34 @@
 #include <iostream>
 #include <algorithm>
 
-int mat[101][101], prt[101] = {0}, dfn[101] = {0}, low[101] = {0};
-int cnt = 0, n, num = 0, ans = 0, root = 0, son = 0;
-bool mk[101] = {false};
+int mat[151][151], prt[151] = {0}, dfn[151] = {0}, low[151] = {0};
+int n, m, num = 0, ans = 0, root = 0, son = 0;
+bool mk[151] = {false};
 
-int read(int &x);
+void read(int &x);
 void write(int x);
 void dfs(int u);
 
 int main(int argc, char const *argv[]) {
-    while (read(n)) {
-        memset(mat, 0, sizeof(mat));
-        memset(prt, 0, sizeof(prt));
-        memset(dfn, 0, sizeof(dfn));
-        memset(low, 0, sizeof(low));
-        memset(mk, false, sizeof(mk));
-        num = ans = root = son = 0;
-        char c;
-        int tmp = 0, dot;
-        while (read(dot)) {
-            while ((c = std::cin.get()) != '\n') {
-                if (c == ' ' && tmp > 0) {
-                    mat[dot][tmp] = mat[tmp][dot] = 1;
-                    tmp = 0;
-                } else if (c != ' ') {
-                    tmp = 10 * tmp + (c - '0');
-                }
-            }
-            mat[dot][tmp] = mat[tmp][dot] = 1;
-            tmp = 0;
-        }
-        dfs(1);
-        if (son == 1) {
-            ans--;
-            mk[root] = false;
-        }
-        write(ans);
-        std::cout.put('\n');
+    memset(mat, 0, sizeof(mat));
+    read(n);
+    read(m);
+    for (int i = 0, a, b; i < m; i++) {
+        read(a);
+        read(b);
+        mat[a][b] = mat[b][a] = 1;
     }
+    dfs(1);
+    if (son == 1) {
+        ans--;
+        mk[root] = false;
+    }
+    write(ans);
+    std::cout.put('\n');
     return 0;
 }
 
-int read(int &x) {
+void read(int &x) {
     x = 0;
     char c = std::cin.get();
     while (c < '0' || c > '9')
@@ -53,7 +39,6 @@ int read(int &x) {
         x = x * 10 + c - '0';
         c = std::cin.get();
     }
-    return x;
 }
 
 void write(int x) {
@@ -78,7 +63,7 @@ void dfs(int u) {
                 prt[v] = u;
                 dfs(v);
                 low[u] = std::min(low[u], low[v]);
-                if (low[v] >= dfn[u]) {
+                if (low[v] > dfn[u]) {
                     if (!mk[u]) {
                         ans++;
                         mk[u] = true;
