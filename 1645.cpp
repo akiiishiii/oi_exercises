d85bc21ebb4e74d1202b0f60c936b073cd513baa
Patrick233 <patrick4dev@foxmail.com>
2018-09-28 22:01:03 +0800

    updates

--- 1645.cpp
+++ 1645.cpp
@@ -1,7 +1,65 @@
 // 1645.cpp
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
+int v[50001] = {0}, prime[50001] = {0};
+int ans, a0, a1, b0, b1, m;
 
+void primes(int n);
+void check(int x);
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int n;
+    std::cin >> n;
+    primes(50000);
+    while (n--) {
+        ans = 1;
+        std::cin >> a0 >> a1 >> b0 >> b1;
+        for (int i = 1; i <= m && ans; i++)
+            if (!(b1 % prime[i]))
+                check(prime[i]);
+        if (b1 > 1)
+            check(b1);
+        std::cout << ans << '\n';
+    }
     return 0;
+}
+
+void primes(int n) {
+    m = 0;
+    for (int i = 2; i <= n; i++) {
+        if (!v[i]) {
+            v[i] = i;
+            prime[++m] = i;
+        }
+        for (int j = 1; j <= m; j++) {
+            if (prime[j] > v[i] || prime[j] > n/i) break;
+            v[i*prime[j]] = prime[j];
+        }
+    }
+}
+
+void check(int x) {
+    int t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+    while (!(a0 % x)) {
+        a0 /= x;
+        t1++;
+    }
+    while (!(a1 % x)) {
+        a1 /= x;
+        t2++;
+    }
+    while (!(b0 % x)) {
+        b0 /= x;
+        t3++;
+    }
+    while (!(b1 % x)) {
+        b1 /= x;
+        t4++;
+    }
+    if (t2 > t1 || t3 > t4 || (t1 > t2 && t4 > t3 && t4 > t2))
+        ans = 0;
+    else if (t1 == t2 && t3 == t4)
+        ans *= t4 - t2 + 1;
 }
--- test.cpp
+++ test.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
+
+std::ofstream out("prime.txt");
+
+// 线性筛法
+int const MAX_N = 50001;
+int v[MAX_N], prime[MAX_N];
+int m;
+void primes(int n) {
+	memset(v, 0, sizeof(v)); // 最小质因子
+	m = 0; // 质数数量
+	for (int i = 2; i <= n; i++) {
+		if (v[i] == 0) { // i是质数
+			v[i] = i;
+			prime[++m] = i;
+		}
+		// 给当前的数i乘上一个质因子
+		for (int j = 1; j <= m; j++) {
+			// i有比prime[j]更小的质因子，或者超出n的范围
+			if (prime[j] > v[i] || prime[j] > n/i) break;
+			// prime[j]是合数i*prime[j]的最小质因子
+			v[i*prime[j]] = prime[j];
+		}
+	}
+	for (int i = 1; i <= m; i++)
+		out << prime[i] << ", ";
+}
 
 int main(int argc, char const *argv[]) {
-    int n[21][21];
-    memset(n, 0, sizeof(n));
-    for (int i = 1; i <= 20; i++)
-        n[i][1] = n[i][i] = 1;
-    for (int i = 2; i <= 20; i++)
-        for (int j = 2; j < i; j++)
-            n[i][j] = n[i - 1][j - 1] + n[i - 1][j];
-    std::cout << "{\n";
-    for (int i = 1; i <= 20; i++) {
-        std::cout << '{';
-        for (int j = 1; j <= 19; j++)
-            std::cout << n[i][j] << ", ";
-        std::cout << n[i][20] << "},\n";
-    }
-    std::cout << "}\n";
-        return 0;
+    std::ios_base::sync_with_stdio(false);
+    out << "int prime[] = {";
+    primes(50000);
+    out << "\b};\n";
+    return 0;
 }
