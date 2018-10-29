878493024e68aa1b1b9041a912d8a239b589a6fd
Patrick233 <patrick4dev@foxmail.com>
2018-10-29 22:24:18 +0800

    updates

--- code/1910.cpp
+++ code/1910.cpp
@@ -1,20 +1,55 @@
 // 1910.cpp
-#include <iostream>
-#include <vector>
-#include <queue>
 #include <functional>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
 
-struct huffman {
+struct s : public std::binary_function<int, int, bool> {
+};
 
-}
+struct huffman_tree {
+    int l, r, v, d, dad;
+} a[105];
+std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> q;
+int n;
+
+void build(int x);
+void print(int x, std::string s);
 
 int main(int argc, char const *argv[]) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    int n, d[51];
     std::cin >> n;
     for (int i = 1; i <= n; i++)
-        std::cin >> d[i];
-
+        std::cin >> a[i].v, q.push(std::make_pair(a[i].v, i));
+    build(n + 1);
+    print(2 * n - 1, "");
     return 0;
+}
+
+void build(int x) {
+    std::pair<int, int> y;
+    y = q.top();
+    q.pop();
+    a[x].l = y.second;
+    a[y.second].dad = x;
+    a[x].v += y.first;
+    y = q.top();
+    q.pop();
+    a[x].r = y.second;
+    a[y.second].dad = x;
+    a[x].v += y.first;
+    q.push(std::make_pair(a[x].v, x));
+    if (x < 2 * n - 1)
+        build(x + 1);
+}
+
+void print(int x, std::string s) {
+    if (x <= n)
+        std::cout << s + '\n';
+    if (a[x].l)
+        print(a[x].l, s + '0');
+    if (a[x].r)
+        print(a[x].r, s + '1');
 }
--- test.cpp
+++ test.cpp
@@ -1,36 +1,13 @@
+#include <iomanip>
 #include <iostream>
-/*
-typedef long motherkiller;
-typedef long long supermotherkiller;
 
-template <typename T>
-class node {
-public:
-    T data;
-    node *parent = nullptr, *lchild = nullptr, *rchild = nullptr;
-};
-
-template <typename T>
-void visit(const node<T> *no) {
-    if (no -> lchild != nullptr)
-        visit(no -> lchild);
-    if (no -> rchild != nullptr)
-        visit(no -> rchild);
-    std::cout << no -> data << '\n';
-    return;
-}
-*/
-int main() {
-    //std::locale::global(std::locale(""));
-    //std::wcout.imbue(std::locale(LC_CTYPE, ""));
-    //setlocale(LC_TIME, "chs" );
-    //std::locale oNewLocale( std::locale(),"",std::locale::ctype );
-    //std::locale oPreviousLocale = std::locale::global( oNewLocale );
-    //...文件打开，保存信息，关闭文件等操作。
-    //std::locale::global( oPreviousLocale );
-    //std::wcout.imbue(std::locale(std::locale(),"",LC_CTYPE));
-    //while (true)
-    //    std::wcout << L"小伙子\n";
-    std::cout << int('0') << ' ' << int('9') << ' ' << int('A');
+int main(int argc, char const *argv[]) {
+    long double price;
+    std::cout << "Please, enter the price: ";
+    std::cin >> std::get_money(price);
+    if (std::cin.fail())
+        std::cout << "Error reading price\n";
+    else
+        std::cout << "The price entered is: " << std::put_money(price) << '\n';
     return 0;
 }
