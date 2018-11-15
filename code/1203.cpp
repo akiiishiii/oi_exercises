a69526feb4fd28ba04be6b5bdf94cda957f94611
Patrick233 <patrick4dev@foxmail.com>
2018-11-15 22:33:27 +0800

    updates

--- code/1203.cpp
+++ code/1203.cpp
@@ -1,26 +1,33 @@
 // 1203.cpp
 #include <iostream>
 
-int const Maxn = 100005;
+int const Maxn = 60005;
 
 struct segment_tree {
     int a, b;
-    int sum, delta;
+    int c, min;
 } tree[4 * Maxn + 5];
 
-void build(int v, int l, int r);
+int c, s, r, minn, dl, dr, dn;
 
+void build(int v, int l, int r);
 void pushdown(int v);
+void search(int v, int l, int r);
+void modify(int v, int l, int r);
 
 int main(int argc, const char * argv[]) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    int c, s, r;
     std::cin >> c >> s >> r;
-
     build(1, 1, c);
     for (int i = 1; i <= r; i++) {
-
+        std::cin >> dl >> dr >> dn;
+        minn = 0x3f3f3f3f;
+        search(1, dl, --dr);
+        if (minn >= dn)
+            std::cout << "YES\n", modify(1, dl, dr);
+        else
+            std::cout << "NO\n";
     }
     return 0;
 }
@@ -28,10 +35,48 @@ int main(int argc, const char * argv[]) {
 void build(int v, int l, int r) {
     tree[v].a = l;
     tree[v].b = r;
-    tree[v].sum = tree[v].delta = 0;
+    tree[v].min = s;
+    tree[v].c = 0;
     if(l == r)
         return;
     int mid = (l + r) >> 1;
     build(v << 1, l, mid);
     build((v << 1) | 1, mid + 1, r);
+}
+
+void pushdown(int v) {
+    if (tree[v].c > 0) {
+        tree[v << 1].min -= tree[v].c;
+        tree[(v << 1) | 1].min -= tree[v].c;
+        tree[v << 1].c += tree[v].c;
+        tree[(v << 1) | 1].c += tree[v].c;
+        tree[v].c = 0;
+    }
+}
+
+void search(int v, int l, int r) {
+    if (l > tree[v].b || r < tree[v].a)
+        return;
+    if (l <= tree[v].a && r >= tree[v].b) {
+        minn = std::min(minn, tree[v].min);
+        return;
+    }
+    pushdown(v);
+    search(v << 1, l, r);
+    search((v << 1) | 1, l, r);
+    tree[v].min = std::min(tree[v << 1].min, tree[(v << 1) | 1].min);
+}
+
+void modify(int v, int l, int r) {
+    if (l > tree[v].b || r < tree[v].a)
+        return;
+    if (l <= tree[v].a && r >= tree[v].b) {
+        tree[v].min -= dn;
+        tree[v].c += dn;
+        return;
+    }
+    pushdown(v);
+    modify(v << 1, l, r);
+    modify((v << 1) | 1, l, r);
+    tree[v].min = std::min(tree[v << 1].min, tree[(v << 1) | 1].min);
 }
---
+++ code/1204.cpp
@@ -0,0 +1,7 @@
+// 1204.cpp
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
