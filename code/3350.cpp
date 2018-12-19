5854a5c34a67bc95c7c0f02815bc510257eaca2c
Patrick233 <patrick4dev@foxmail.com>
2018-12-19 15:36:02 +0800

    2018年12月19日

---
+++ code/3350.cpp
@@ -0,0 +1,16 @@
+// 3350.cpp
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+int const Maxn = 50001;
+
+struct dat {
+    int l, r, id;
+    long long a, b;
+} ds[Maxn];
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
