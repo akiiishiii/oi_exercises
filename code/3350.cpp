bcedfb33c737698846c4614b364af47d38023cd6
Patrick233 <patrick4dev@foxmail.com>
2018-12-19 15:36:17 +0800

    2018年12月19日

--- code/3350.cpp
+++ code/3350.cpp
@@ -4,11 +4,14 @@
 #include <functional>
 
 int const Maxn = 50001;
-
+int n, m, pos[Maxn], c[Maxn];
+long long s[Maxn], ans;
 struct dat {
     int l, r, id;
     long long a, b;
 } ds[Maxn];
+struct comp
+
 
 int main(int argc, char const *argv[]) {
 
