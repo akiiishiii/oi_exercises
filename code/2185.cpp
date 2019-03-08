50c9dd2c2507173f01888bf0ff5072f7f100b219
Patrick233 <patrick4dev@foxmail.com>
2019-03-08 16:56:44 +0800

    updates

---
+++ code/2185.cpp
@@ -0,0 +1,59 @@
+//2185.cpp
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+int n;
+double f[21], a[21][21];
+
+inline double sqr(double x) { return x * x; }
+bool gauss();
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cin >> n;
+    for (int i = 1; i <= n; i++)
+        std::cin >> f[i];
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) {
+            double t;
+            std::cin >> t;
+            a[i][j] = 2 * (t - f[j]);
+            a[i][n + 1] += sqr(t) - sqr(f[j]);
+        }
+    gauss();
+    for (int i = 1; i <= n; i++)
+        std::cout << std::fixed << std::setprecision(3) << a[i][n + 1] << ' ';
+    std::cout << '\n';
+    return 0;
+}
+
+bool gauss() {
+    int now = 1, to;
+    double t;
+    for (int i = 1; i <= n; i++) {
+        for (to = now; to <= n; to++)
+            if (fabs(a[to][i]) > 1e-6)
+                break;
+        if (to > n)
+            continue;
+        if (to != now)
+            for (int j = 1; j <= n + 1; j++)
+                std::swap(a[to][j], a[now][j]);
+        t = a[now][i];
+        for (int j = 1; j <= n + 1; j++)
+            a[now][j] /= t;
+        for (int j = 1; j <= n; j++)
+            if (j != now) {
+                t = a[j][i];
+                for (int k = 1; k <= n + 1; k++)
+                    a[j][k] -= t * a[now][k];
+            }
+        now++;
+    }
+    for (int i = now; i <= n; i++)
+        if (fabs(a[i][n + 1]) > 1e-6)
+            return false;
+    return true;
+}
---
+++ code/2574.cpp
@@ -0,0 +1,91 @@
+// 2574.cpp
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+double a[105][105], ans[105];
+int n;
+
+void simplify(int l);
+int check();
+int Gauss();
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cin >> n;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n + 1; j++)
+            std::cin >> a[i][j];
+    int res = Gauss();
+    if (res != 1) {
+        std::cout << res << '\n';
+        return 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        std::cout << 'x' << i << '=';
+        if (fabs(ans[i]) <= 1e-7)
+            std::cout << "0\n";
+        else
+            std::cout << std::fixed << std::setprecision(2) << ans[i] << '\n';
+    }
+    return 0;
+}
+
+void simplify(int l) {
+    int r = l;
+    double Max = fabs(a[r][l]);
+    for (int i = l + 1; i <= n; i++)
+        if (fabs(a[i][l]) > Max) {
+            Max = fabs(a[i][l]);
+            r = i;
+        }
+    if (r != l)
+        for (int i = l; i <= n + 1; i++)
+            std::swap(a[l][i], a[r][i]);
+    for (int i = l + 1; i <= n; i++) {
+        if (fabs(a[l][l]) <= 1e-7)
+            continue;
+        double mul = a[i][l] / a[l][l];
+        for (int j = l; j <= n + 1; j++)
+            a[i][j] -= a[l][j] * mul;
+    }
+}
+
+int check() {
+    for (int i = 1; i <= n; i++) {
+        bool b = false;
+        for (int j = 1; j <= n; j++)
+            if (fabs(a[i][j]) > 1e-7) {
+                b = true;
+                break;
+            }
+        if (!b) {
+            if (fabs(a[i][n + 1]) <= 1e-7)
+                return 0;
+            else
+                return -1;
+        }
+    }
+    return 1;
+}
+
+int Gauss() {
+    for (int i = 1; i <= n; i++)
+        simplify(i);
+    int res = check();
+    if (res != 1)
+        return res;
+    for (int i = n; i >= 1; i--) {
+        for (int j = i + 1; j <= n; j++)
+            a[i][n + 1] -= a[i][j] * ans[j];
+        if (fabs(a[i][i]) <= 1e-7) {
+            if (fabs(a[i][n + 1]) <= 1e-7)
+                return 0;
+            else
+                return -1;
+        }
+        ans[i] = a[i][n + 1] / a[i][i];
+    }
+    return 1;
+}
