a6a17d15397dc66fddf565fd3efd3f2c0056a7ff
Patrick233 <patrick4dev@foxmail.com>
2019-03-10 22:28:39 +0800

    updates

--- Beep/Beep.cpp
+++ Beep/Beep.cpp
@@ -1,60 +1,65 @@
-#include<windows.h>
-#include<fstream>
-#include"Music.hpp"
+#include "Music.hpp"
+#include <fstream>
+#include <windows.h>
 using namespace std;
 fstream f;
-int main(int count,char **chars)
-{f.open(chars[1]);
- if(!f)
-   {if(count<3)cout<<"打开文件失败";
-    return -1;
-   }
- {string s="",sin;
-  while(f>>sin)s+=sin;
-  f.close(); 
-  int c;
-  for(c=0;;c++)
-  {if(s[c]=='`')break;
-   cout<<s[c];
-  }
-  int tl=0;
-  for(c++;s[c]!='`';c++)
-     {tl*=10;
-      tl+=s[c]-'0';
-	 } 
-  int len=0;
-  for(int i=c;i<s.length();i++)
-     if(s[i]>='0'&&s[i]<='7')len++;
-  cout<<len<<endl;
-  Music M(len);
-  for(int i=0;i<len;i++)
-     {int t=tl,add=0;
-      double so=1;
-      while(1)
-      {c++;
-	   if(s[c]=='+')
-         {so/=2;
-	 	 }
-	   if(s[c]=='-')
-	     {add+=tl;
-	     }
-	   if(s[c]=='/')
-	     {t/=2;
-		 }
-	   if(s[c]=='*')
-	     {so*=2;
-		 }
-	   if(s[c]>='0'&&s[c]<='7')
-	     {DWORD sou=so*sound[s[c]-'0'].Hz;
-	      Sound soun;
-	      soun.Hz=sou;
-	      int ft=t+add;
-	      M.Set(i,soun,ft);
-		  break;
-		 }
-	  }
-	 }
-  M.Play();
- }
- return 0;
+int main(int count, char **chars) {
+    f.open(chars[1]);
+    if (!f) {
+        if (count < 3)
+            cout << "鎵撳紑鏂囦欢澶辫触";
+        return -1;
+    }
+    {
+        string s = "", sin;
+        while (f >> sin)
+            s += sin;
+        f.close();
+        int c;
+        for (c = 0;; c++) {
+            if (s[c] == '`')
+                break;
+            cout << s[c];
+        }
+        int tl = 0;
+        for (c++; s[c] != '`'; c++) {
+            tl *= 10;
+            tl += s[c] - '0';
+        }
+        int len = 0;
+        for (int i = c; i < s.length(); i++)
+            if (s[i] >= '0' && s[i] <= '7')
+                len++;
+        cout << len << endl;
+        Music M(len);
+        for (int i = 0; i < len; i++) {
+            int t = tl, add = 0;
+            double so = 1;
+            while (1) {
+                c++;
+                if (s[c] == '+') {
+                    so /= 2;
+                }
+                if (s[c] == '-') {
+                    add += tl;
+                }
+                if (s[c] == '/') {
+                    t /= 2;
+                }
+                if (s[c] == '*') {
+                    so *= 2;
+                }
+                if (s[c] >= '0' && s[c] <= '7') {
+                    DWORD sou = so * sound[s[c] - '0'].Hz;
+                    Sound soun;
+                    soun.Hz = sou;
+                    int ft = t + add;
+                    M.Set(i, soun, ft);
+                    break;
+                }
+            }
+        }
+        M.Play();
+    }
+    return 0;
 }
--- Beep/Music.hpp
+++ Beep/Music.hpp
@@ -1,54 +1,38 @@
-#include<Windows.h>
-#include<iostream>
+#include <Windows.h>
+#include <iostream>
 using namespace std;
-struct Sound
-{DWORD Hz;
- void beep(int len)
- {Beep(Hz,len);
- }
+struct Sound {
+    DWORD Hz;
+    void beep(int len) { Beep(Hz, len); }
 };
 
-Sound
-do_[3]={{128},{256},{512}},
-re_[3]={{144},{288},{576}},
-mi_[3]={{160},{320},{640}},
-fa_[3]={{171},{341},{683}},
-so_[3]={{192},{384},{768}},
-la_[3]={{213},{427},{853}},
-si_[3]={{240},{480},{960}};
-Sound sound[8]=
-{{0},
- {256},
- {288},
- {320},
- {341},
- {384},
- {427},
- {480}
+Sound do_[3] = {{128}, {256}, {512}}, re_[3] = {{144}, {288}, {576}},
+      mi_[3] = {{160}, {320}, {640}}, fa_[3] = {{171}, {341}, {683}},
+      so_[3] = {{192}, {384}, {768}}, la_[3] = {{213}, {427}, {853}},
+      si_[3] = {{240}, {480}, {960}};
+Sound sound[8] = {{0}, {256}, {288}, {320}, {341}, {384}, {427}, {480}};
+struct Music {
+    Sound *s;
+    int lenth;
+    DWORD *len;
+    Music(int Lent) {
+        lenth = Lent;
+        s = new Sound[lenth];
+        len = new DWORD[lenth];
+    }
+    ~Music() {
+        delete[] s;
+        delete[] len;
+    }
+    void Set(int pos_from_0, Sound S, DWORD lent) {
+        *(s + pos_from_0) = S;
+        *(len + pos_from_0) = lent;
+    }
+    void Play() {
+        for (int i = 0; i < lenth; i++) {
+            Beep(s[i].Hz, len[i]);
+            cout << s[i].Hz << " " << len[i] << endl;
+            // Sleep(len[i]);
+        }
+    }
 };
-struct Music
-{Sound *s;
- int lenth;
- DWORD *len;
- Music(int Lent)
- {lenth=Lent;
-  s=new Sound[lenth];
-  len=new DWORD[lenth];
- }
- ~Music()
- {delete[]s;
-  delete[]len;
- }
- void Set(int pos_from_0,Sound S,DWORD lent)
- {*(s+pos_from_0)=S;
-  *(len+pos_from_0)=lent;
- }
- void Play()
- {for(int i=0;i<lenth;i++)
-     {Beep(s[i].Hz,len[i]);
-     cout<<s[i].Hz<<" "<<len[i]<<endl;
-      //Sleep(len[i]); 
-	 }
- }
-};
-
