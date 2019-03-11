a44367ce183901925663e783f5db63b6b108c18b
Patrick233 <patrick4dev@foxmail.com>
2019-03-11 22:27:13 +0800

    updates

--- code/1533.cpp
+++ code/1533.cpp
@@ -1,101 +1,94 @@
 // 1533.cpp
 #include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iomanip>
 #include <iostream>
-#include <queue>
-#include <vector>
-#define maxn 100005 * 20
-#define inf 1e9
-#define ll long long
-using namespace std;
 
-struct Ares {
-    int lc, rc;
-    int num;
-} t[maxn];
+int const Maxn = 100005 * 20;
+int n, num;
+int a[Maxn], b[Maxn], root[Maxn], ans[Maxn], cnt = 0;
+int head[Maxn], ver[Maxn << 1], Next[Maxn << 1], tot = 0;
 
-struct tree {
-    int to, next;
-} xm[maxn];
+struct segment_tree {
+    int lc, rc, num;
+} t[Maxn];
 
-int n, N;
-int a[maxn], b[maxn], rt[maxn], ans[maxn], tot = 0;
-int h[maxn] = {0}, cnt = 0;
-void Addedge(int x, int y) {
-    cnt++;
-    xm[cnt].next = h[x], xm[cnt].to = y;
-    h[x] = cnt;
+
+inline void add(int x, int y) { ver[++tot] = y, Next[tot] = head[x], head[x] = tot; }
+void pushup(int x);
+void insert(int &x, int l, int r, int d);
+int mesh(int x, int l, int r, int ll, int rr);
+int merge(int x, int y);
+void dfs(int x);
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cin >> n;
+    for (int i = 1; i <= n; i++)
+        std::cin >> a[i], b[i] = a[i];
+    std::sort(b + 1, b + 1 + n);
+    num = std::unique(b + 1, b + 1 + n) - b - 1;
+    for (int i = 1; i <= n; i++)
+        a[i] = std::lower_bound(b + 1, b + 1 + num, a[i]) - b;
+    for (int i = 2, x; i <= n; i++)
+        std::cin >> x, add(x, i);
+    for (int i = 1; i <= n; i++)
+        insert(root[i], 1, n, a[i]);
+    dfs(1);
+    for (int i = 1; i <= n; i++)
+        std::cout << ans[i] << '\n';
+    return 0;
 }
-void Pushup(int x) {
+
+
+void pushup(int x) {
     int lc = t[x].lc, rc = t[x].rc;
     t[x].num = t[lc].num + t[rc].num;
 }
-void Insert(int &x, int l, int r, int helios) {
+
+void insert(int &x, int l, int r, int d) {
     if (l > r)
         return;
-    x = ++tot;
+    x = ++cnt;
     if (l == r) {
         t[x].num = 1;
         return;
     }
-    int mid = (l + r) / 2;
-    if (helios <= mid)
-        Insert(t[x].lc, l, mid, helios);
+    int mid = (l + r) >> 1;
+    if (d <= mid)
+        insert(t[x].lc, l, mid, d);
     else
-        Insert(t[x].rc, mid + 1, r, helios);
-    Pushup(x);
+        insert(t[x].rc, mid + 1, r, d);
+    pushup(x);
 }
-int Artemis(int x, int l, int r, int pl, int pr) {
+
+int mesh(int x, int l, int r, int ll, int rr) {
     int ans = 0;
-    if (pr < pl)
+    if (rr < ll)
         return 0;
-    if (pl <= l && r <= pr)
+    if (ll <= l && r <= rr)
         return t[x].num;
-    int mid = (l + r) / 2;
-    if (pl <= mid)
-        ans += Artemis(t[x].lc, l, mid, pl, pr);
-    if (pr > mid)
-        ans += Artemis(t[x].rc, mid + 1, r, pl, pr);
+    int mid = (l + r) >> 1;
+    if (ll <= mid)
+        ans += mesh(t[x].lc, l, mid, ll, rr);
+    if (rr > mid)
+        ans += mesh(t[x].rc, mid + 1, r, ll, rr);
     return ans;
 }
-int Merge(int x, int y) {
+
+int merge(int x, int y) {
     if (!x || !y)
         return x | y;
-    t[x].lc = Merge(t[x].lc, t[y].lc);
-    t[x].rc = Merge(t[x].rc, t[y].rc);
-    Pushup(x);
+    t[x].lc = merge(t[x].lc, t[y].lc);
+    t[x].rc = merge(t[x].rc, t[y].rc);
+    pushup(x);
     return x;
 }
+
 void dfs(int x) {
-    int i, y;
-    for (i = h[x]; i; i = xm[i].next) {
-        y = xm[i].to;
+    for (int i = head[x]; i; i = Next[i]) {
+        int y = ver[i];
         dfs(y);
-        rt[x] = Merge(rt[x], rt[y]);
+        root[x] = merge(root[x], root[y]);
     }
-    ans[x] = Artemis(rt[x], 1, n, a[x] + 1, N);
-}
-int main() {
-    int i, x;
-    n = read();
-    for (i = 1; i <= n; i++)
-        a[i] = b[i] = read();
-    sort(b + 1, b + 1 + n);
-    N = unique(b + 1, b + 1 + n) - b - 1;
-    for (i = 1; i <= n; i++)
-        a[i] = lower_bound(b + 1, b + 1 + N, a[i]) - b;
-    for (i = 2; i <= n; i++) {
-        scanf("%d", &x);
-        Addedge(x, i);
-    }
-    for (i = 1; i <= n; i++)
-        Insert(rt[i], 1, n, a[i]);
-    dfs(1);
-    for (i = 1; i <= n; i++)
-        printf("%d\n", ans[i]);
-    return 0;
+    ans[x] = mesh(root[x], 1, n, a[x] + 1, num);
 }
--- code/4362.cpp
+++ code/4362.cpp
@@ -1,10 +1,41 @@
 // 4362.cpp
 #include <iostream>
+#include <algorithm>
+
+int const Maxn = 1010;
+double const eps = 1e-5;
+int n, m, ans = 0, cnt = 0, pos[Maxn];
+
+struct linear_basis {
+    int money;
+    double a[Maxn];
+    bool operator<(linear_basis const &l) const { return money < l.money; }
+} p[Maxn];
 
 int main(int argc, char const *argv[]) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    std::cin >> nmsl;
-    long long nmsl;
+    std::cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            std::cin >> p[i].a[j];
+    for (int i = 1; i <= n; i++)
+        std::cin >> p[i].money;
+    std::sort(p + 1, p + n + 1);
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            if (std::abs(p[i].a[j]) > eps) {
+                if (!pos[j]) {
+                    pos[j] = i;
+                    cnt++;
+                    ans += p[i].money;
+                    break;
+                } else {
+                    double e = p[i].a[j] / p[pos[j]].a[j];
+                    for (int k = 1; k <= m; k++)
+                        p[i].a[k] -= e * p[pos[j]].a[k];
+                }
+            }
+    std::cout << cnt << ' ' << ans << '\n';
     return 0;
 }
