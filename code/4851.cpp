01f54e2d632d4b5183fe65014b871fc3de7e4104
Patrick233 <patrick4dev@foxmail.com>
2019-03-13 22:33:44 +0800

    update

--- code/4851.cpp
+++ code/4851.cpp
@@ -1,64 +1,61 @@
 // 4851.cpp
 #include <iostream>
+#include <queue>
 
-int const Maxn = 100010;
-int n, m, tot;
-int head[Maxn], ver[Maxn << 1], Next[Maxn << 1];
-int son[Maxn], maxd[Maxn], top[Maxn], dep[Maxn], prt[Maxn];
+int const Maxn = 200010;
+long long n, m, tot, ans = 0;
+long long head[Maxn], ver[Maxn << 1], Next[Maxn << 1];
+long long son[Maxn], maxd[Maxn], top[Maxn], prt[Maxn], val[Maxn];
+std::priority_queue<long long> q;
 
-inline void add(int x, int y) { ver[++tot] = y, Next[tot] = head[x], head[x] = tot; }
-void dfs1(int x);
-void dfs2(int x, int tp);
-int lca(int x, int y);
+inline void add(long long x, long long y) { ver[++tot] = y, Next[tot] = head[x], head[x] = tot; }
+void dfs1(long long x);
+void dfs2(long long x, long long tp);
 
 int main(int argc, char const *argv[]) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cin >> n >> m;
-    for (int i = 1, x, y; i < n; i++) {
-        std::cin >> x >> y;
-        add(x, y), add(y, x);
-    }
+    for (int i = 1; i <= n; i++)
+        std::cin >> val[i];
+    for (int i = 1, x, y; i < n; i++)
+        std::cin >> x >> y, add(x, y);
     dfs1(1);
     dfs2(1, 1);
-    for (int i = 1, x, y; i <= m; i++) {
-        std::cin >> x >> y;
-        std::cout << lca(x, y) << '\n';
+    for (int i = 1; i <= n; i++)
+        if (top[i] == i)
+            q.push(maxd[i]);
+    for (int i = 1; i <= m; i++) {
+        ans += q.top();
+        q.pop();
+        if (q.empty())
+            break;
     }
+    std::cout << ans << '\n';
     return 0;
 }
 
-void dfs1(int x) {
-    dep[x] = dep[prt[x]] + 1;
-    maxd[x] = dep[x];
-    for (int i = head[x]; i; i = Next[i]) {
-        int y = ver[i];
+void dfs1(long long x) {
+    maxd[x] = val[x];
+    for (long long i = head[x]; i; i = Next[i]) {
+        long long y = ver[i];
         if (y != prt[x]) {
             prt[y] = x;
             dfs1(y);
-            maxd[x] = std::max(maxd[y], maxd[x]);
+            maxd[x] = std::max(maxd[y] + val[x], maxd[x]);
             if (maxd[y] > maxd[son[x]])
                 son[x] = y;
         }
     }
 }
 
-void dfs2(int x, int tp) {
+void dfs2(long long x, long long tp) {
     top[x] = tp;
     if (son[x])
         dfs2(son[x], tp);
-    for (int i = head[x]; i; i = Next[i]) {
-        int y = ver[i];
+    for (long long i = head[x]; i; i = Next[i]) {
+        long long y = ver[i];
         if (y != prt[x] && y != son[x])
             dfs2(y, y);
     }
-}
-
-int lca(int x, int y) {
-    while (top[x] != top[y]) {
-        if (dep[top[x]] < dep[top[y]])
-            std::swap(x, y);
-        x = prt[top[x]];
-    }
-    return dep[x] < dep[y] ? x : y;
 }
