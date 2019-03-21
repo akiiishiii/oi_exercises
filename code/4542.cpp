ab9ebb41c80fc234eb4e4b4328bc538c99d40f0f
Patrick233 <patrick4dev@foxmail.com>
2019-03-21 16:46:32 +0800

    updates

---
+++ code/4542.cpp
@@ -0,0 +1,51 @@
+// 4542.cpp
+#include <iostream>
+
+long long const Mod = 10007;
+
+long long power(long long a, long long b);
+inline long long inv(long long x) { return power(x, Mod - 2); }
+long long C(long long n, long long m);
+long long lucas(long long n, long long m);
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    long long T, n, m;
+    std::cin >> T;
+    while (T--) {
+        std::cin >> n >> m;
+        std::cout << lucas(n, m) << '\n';
+    }
+    return 0;
+}
+
+long long power(long long a, long long b) {
+    long long ans = 1;
+    while (b) {
+        if (b & 1)
+            ans = (ans * a) % Mod;
+        b >>= 1;
+        a = (a * a) % Mod;
+    }
+    return ans;
+}
+
+long long C(long long n, long long m) {
+    if (n < m)
+        return 0;
+    long long up = 1, down = 1;
+    for (long long i = n - m + 1; i <= n; ++i)
+        up = up * i % Mod;
+    for (long long i = 1; i <= m; ++i)
+        down = down * i % Mod;
+    return up * inv(down) % Mod;
+}
+
+long long lucas(long long n, long long m) {
+    if (m > n)
+        return 0;
+    if (m == 0)
+        return 1;
+    return C(n % Mod, m % Mod) % Mod * lucas(n / Mod, m / Mod) % Mod;
+}
--- code/test.cpp
+++ code/test.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 
-long long x, xx, y, yy, p, facts[200010];
+long long const Mod = 10007;
 
 long long power(long long a, long long b);
-long long inv(long long x) { return power(x, p - 2); }
+inline long long inv(long long x) { return power(x, Mod - 2); }
 long long C(long long n, long long m);
 long long lucas(long long n, long long m);
 
-int main() {
+int main(int argc, char const *argv[]) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    while (std::cin >> x >> y >> xx >> yy >> p) {
-        facts[0] = 1;
-        long long cnt = std::min(p, std::max(yy + 5, xx + 5));
-        for (long long i = 1; i <= cnt; i++)
-            facts[i] = (facts[i - 1] * i) % p;
-        long long ans = 0;
-        for (long long j = y + 1; j <= yy + 1; ++j) {
-            ans = (ans + lucas(xx + 1, j) - lucas(x, j)) % p;
-        }
-        std::cout << ans << '\n';
+    long long T, n, m;
+    std::cin >> T;
+    while (T--) {
+        std::cin >> n >> m;
+        std::cout << lucas(n, m) << '\n';
     }
     return 0;
 }
@@ -28,9 +23,9 @@ long long power(long long a, long long b) {
     long long ans = 1;
     while (b) {
         if (b & 1)
-            ans = (ans * a) % p;
+            ans = (ans * a) % Mod;
         b >>= 1;
-        a = (a * a) % p;
+        a = (a * a) % Mod;
     }
     return ans;
 }
@@ -38,7 +33,12 @@ long long power(long long a, long long b) {
 long long C(long long n, long long m) {
     if (n < m)
         return 0;
-    return facts[n] * inv(facts[m]) % p * inv(facts[n - m]) % p;
+    long long up = 1, down = 1;
+    for (long long i = n - m + 1; i <= n; ++i)
+        up = up * i % Mod;
+    for (long long i = 1; i <= m; ++i)
+        down = down * i % Mod;
+    return up * inv(down) % Mod;
 }
 
 long long lucas(long long n, long long m) {
@@ -46,5 +46,5 @@ long long lucas(long long n, long long m) {
         return 0;
     if (m == 0)
         return 1;
-    return C(n % p, m % p) % p * lucas(n / p, m / p) % p;
+    return C(n % Mod, m % Mod) % Mod * lucas(n / Mod, m / Mod) % Mod;
 }
