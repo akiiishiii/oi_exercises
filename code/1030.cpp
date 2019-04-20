a428d69462235b0e227b7ce3132685707a097a32
Patrick233 <patrick4dev@foxmail.com>
2019-04-20 10:35:35 +0800

    updates

--- code/1030.cpp
+++ code/1030.cpp
@@ -1,2 +1,31 @@
 // 1030.cpp
 #include <iostream>
+
+int const Maxn = 100010;
+int n, t, a[Maxn];
+long long s[Maxn], f[Maxn];
+int head = 1, tail = 1;
+long long q[Maxn];
+
+inline long double slope(int j, int k) {
+    return ((f[j] - s[j] * j) - (f[k] - s[k] * k)) / double(k - j);
+}
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cin >> n >> t;
+    for (int i = 1; i <= n; i++)
+        std::cin >> a[i], s[i] = s[i - 1] + a[i];
+    f[0] = q[1] = 0;
+    for (int i = 1; i <= n; i++) {
+        while (head < tail && (slope(q[head], q[head + 1]) <= s[i]))
+            head++;
+        f[i] = f[q[head]] + (s[i] - s[q[head]]) * q[head] - t;
+        while (head < tail && slope(q[tail - 1], q[tail]) >= slope(q[tail], i))
+            tail--;
+        q[++tail] = i;
+    }
+    std::cout << f[n] << '\n';
+    return 0;
+}
--- code/1030copy.cpp
+++ code/1030copy.cpp
@@ -1,33 +1,50 @@
-#include <deque>
+
+#include <cstdio>
 #include <iostream>
-
+using namespace std;
 int n, t, a[100005];
-long long sum[100005], f[100005];
-std::deque<long long> q;
-
-inline long double slope(int j, int k) {
+long long sum[100005], f[100005], q[100005], head, tail;
+long long read() {
+    long long s = 0;
+    char ch = getchar();
+    int f = 1;
+    while (ch < '0' || ch > '9')
+        f = (ch == '-' ? -1 : 1), ch = getchar();
+    while (ch >= '0' && ch <= '9')
+        s = s * 10 + ch - '0', ch = getchar();
+    return s * f;
+}
+void write(long long s) {
+    if (s < 0) {
+        putchar('-');
+        write(-s);
+        return;
+    }
+    if (s > 9)
+        write(s / 10);
+    putchar(s % 10 + '0');
+}
+long double s(int j, int k) {
     return ((f[j] - sum[j] * j) - (f[k] - sum[k] * k)) / ((long double)(k - j));
 }
-
-int main(int argc, char const *argv[]) {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cin >> n >> t;
-    for (int i = 1; i <= n; i++)
-        std::cin >> a[i], sum[i] = sum[i - 1] + a[i];
-    f[0] = 0;
+int main() {
+    n = read();
+    t = read();
     for (int i = 1; i <= n; i++) {
-        while (q.size() && (slope(q.front(), *(q.begin() + 1)) <= sum[1]))
-            q.pop_front();
-        if (q.size())
-            f[i] = f[q.front()] + (sum[i] - sum[q.front()]) * q.front() - t;
-        while (q.size() &&
-               slope(*(q.end() - 1), q.back()) >= slope(q.back(), i))
-            q.pop_back();
-        q.push_back(i);
+        a[i] = read();
+        sum[i] = sum[i - 1] + a[i];
     }
-    std::cout << f[n] << '\n';
+    f[0] = 0;
+    head = tail = 1;
+    q[head] = 0;
+    for (int i = 1; i <= n; i++) {
+        while (head < tail && (s(q[head], q[head + 1]) <= sum[i]))
+            head++;
+        f[i] = f[q[head]] + (sum[i] - sum[q[head]]) * q[head] - t;
+        while (head < tail && s(q[tail - 1], q[tail]) >= s(q[tail], i))
+            tail--;
+        q[++tail] = i;
+    }
+    write(f[n]);
     return 0;
 }
-
-// https://blog.csdn.net/sin_Yang/article/details/82973013
