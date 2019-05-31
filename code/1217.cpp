8881a2518a8c69f0dac200954ade8bc446728568
Patrick233 <patrick4dev@foxmail.com>
2019-05-31 21:29:50 +0800

    updates

---
+++ code/1217.cpp
@@ -0,0 +1,35 @@
+// 1217.cpp
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+
+int a[50001] = {0}, maxf[50001][16], minf[50001][16];
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int n, k;
+    std::cin >> n >> k;
+    memset(maxf, 0, sizeof maxf);
+    memset(minf, 0, sizeof minf);
+    for (int i = 1; i <= n; i++) {
+        std::cin >> a[i];
+        minf[i][0] = maxf[i][0] = a[i];
+    }
+    for (int j = 1; (1 << j) <= n; j++)
+        for (int i = 1; i + (1 << j) - 1 <= n; i++) {
+            maxf[i][j] =
+                std::max(maxf[i][j - 1], maxf[i + (1 << (j - 1))][j - 1]);
+            minf[i][j] =
+                std::min(minf[i][j - 1], minf[i + (1 << (j - 1))][j - 1]);
+        }
+    for (int i = 1, l, r, x; i <= k; i++) {
+        std::cin >> l >> r;
+        x = int(log(r - l + 1) / log(2));
+        std::cout << std::max(maxf[l][x], maxf[r - (1 << x) + 1][x]) -
+                         std::min(minf[l][x], minf[r - (1 << x) + 1][x])
+                  << '\n';
+    }
+    return 0;
+}
---
+++ code/1939.cpp
@@ -0,0 +1,28 @@
+// 1939.cpp
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+
+int a[1000001] = {0}, f[1000001][21];
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int n, k;
+    std::cin >> n >> k;
+    memset(f, 0, sizeof f);
+    for (int i = 1; i <= n; i++) {
+        std::cin >> a[i];
+        f[i][0] = a[i];
+    }
+    for (int j = 1; (1 << j) <= n; j++)
+        for (int i = 1; i + (1 << j) - 1 <= n; i++)
+            f[i][j] = std::max(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
+    for (int i = 1, l, r, x; i <= k; i++) {
+        std::cin >> l >> r;
+        x = int(log2(r - l + 1));
+        std::cout << std::max(f[l][x], f[r - (1 << x) + 1][x]) << '\n';
+    }
+    return 0;
+}
