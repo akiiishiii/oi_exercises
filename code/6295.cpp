3eb03498793632da17df3d2b5823e885ff0a3f9c
Patrick233 <patrick4dev@foxmail.com>
2019-10-03 21:31:13 +0800

    updates

---
+++ code/6295.cpp
@@ -0,0 +1,9 @@
+// 6295.cpp
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << "nan\n";
+    return 0;
+}
---
+++ code/6297.cpp
@@ -0,0 +1,9 @@
+// 6297.cpp
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    return 0;
+}
